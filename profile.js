71abf4f660ab6a03519d6b3c8bb6dba721966650
Shaun VanWeelden <shaun.thomas.3141@gmail.com>
2015-03-06 21:49:55 -0800

    Added initial login functionality

---
+++ profile.js
@@ -0,0 +1,17 @@
+currentUser = undefined;
+
+$("#loginbtn").click(function(){
+    Parse.FacebookUtils.logIn(null, {
+      success: function(user) {
+        if (!user.existed()) {
+          alert("User signed up and logged in through Facebook!");
+        } else {
+          alert("User logged in through Facebook!");
+        }
+        currentUser = user;
+      },
+      error: function(user, error) {
+        alert("User cancelled the Facebook login or did not fully authorize.");
+      }
+    });
+})
