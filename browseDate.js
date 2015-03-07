99cd91a707337161e9d0581a6258db0d9a159440
xolliehoo <facebook@loaner-C02KH039FJ47.local>
2015-03-07 06:49:45 -0800

    date query 1

--- browseDate.js
+++ browseDate.js
@@ -3,15 +3,42 @@ navigator.geolocation.getCurrentPosition(function(position) {
 	currentUser.position = position; 
 });
 
-var userQuery = new Parse.Query(Parse.UserInformation);
-userQuery.equalTo("gender", currentUser.get("genderPref"));
-userQuery.greaterThanOrEqualTo("age", currentUser.get("minAge"));
-userQuery.lessThanOrEqualTo("age", currentUser.get("maxAge"));
+var findDates = function() {
+	var userQuery = new Parse.Query(Parse.UserInformation);
+	userQuery.equalTo("gender", currentUser.get("genderPref"));
+	userQuery.greaterThanOrEqualTo("age", currentUser.get("minAge"));
+	userQuery.lessThanOrEqualTo("age", currentUser.get("maxAge"));
+	userQuery.find({
+		success: displayDates,
+		error: alert("Problem with query.")
+	});
+	/*
+	var DateInfo = Parse.Object.extend("Date");
+	var dateQuery = new Parse.Query(DateInfo);
+	dateQuery.matchesKeyInQuery("userID", "username", userQuery);
+	var userGeo = new Parse.GeoPoint({latitude: currentUser.position.coords.latitude, longitude: currentUser.position.coords.longitude});
+	dateQuery.withinMiles("locationGeo", userGeo, currentUser.get("distancePref"));
+	dateQuery.greaterThanOrEqualTo("dateTime", $("#timePref").val());
+	dateQuery.equalTo("category", $("#categoryPref").val());
+	dateQuery.equalTo("gender", currentUser.get("gender"));
+	dateQuery.greaterThanOrEqualTo("minAge", currentUser.get("age"));
+	dateQuery.lessThanOrEqualTo("maxAge", currentUser.get("age"));
+	dateQuery.find({
+		success: displayDates,
+		error: alert("Problem with query.")
+	});
+	*/
+};
 
+var displayDates = function(results) {
+	if (results.length < 1) {
+		$("#results").html('<p>No results found. Please broaden your filters.</p>');
+	} else {
+		$("#results").html('<p>' + results.length + 'results found!</p>');
+	}
+};
 
-var DateInfo = Parse.Object.extend("Date");
-var dateQuery = new Parse.Query(DateInfo);
-dateQuery.matchesKeyInQuery("userID", "username", userQuery);
-var userGeo = new Parse.GeoPoint({latitude: currentUser.position.coords.latitude, longitude: currentUser.position.coords.longitude});
-dateQuery.withinMiles("locationGeo", userGeo, currentUser.get("distancePref"));
-
+$(function() {
+	$("#distancePref, #timePref, #categoryPref").on("change", findDates);
+	findDates();
+});
