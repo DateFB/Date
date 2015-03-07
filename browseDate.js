f15a5481d691840cbb28ffa0e9458119ca87f9a7
Shaun VanWeelden <shaun.thomas.3141@gmail.com>
2015-03-07 07:49:25 -0800

    small syntax error fix

--- browseDate.js
+++ browseDate.js
@@ -9,35 +9,33 @@ var findDates = function() {
     
     // get user info
     if (currentUser) {
-        
+
         var UserInformationClass = Parse.Object.extend("UserInformation");
         var query = new Parse.Query(UserInformationClass);
         query.equalTo("fbUserName", currentUser.attributes.username);
         query.find({
-          success: function(results) {
-              userInfo = results[0];
-              
+            success: function(results) {
+                userInfo = results[0];
+
                 var userQuery = new Parse.Query(Parse.Object.extend("UserInformation"));
-                
+
                 userQuery.equalTo("gender", userInfo.get("genderPref"));
-//                userQuery.greaterThanOrEqualTo("age", userInfo.get("minAge"));
-//                userQuery.lessThanOrEqualTo("age", userInfo.get("maxAge"));
+                //                userQuery.greaterThanOrEqualTo("age", userInfo.get("minAge"));
+                //                userQuery.lessThanOrEqualTo("age", userInfo.get("maxAge"));
 
                 userQuery.find({
-                    success: function(results) {
-            
-                       console.log(results[0]);
+                success: function(results) {
 
-                   
-                  },
-                  error: function(error) {
+                    console.log(results[0]);
+                },
+                error: function(error) {
                     alert("Error: " + error.code + " " + error.message);
-                  }
+                }
                 });
-          }
-          error: function(error) {
-            alert("Error: " + error.code + " " + error.message);
-          }
+            },
+            error: function(error) {
+                alert("Error: " + error.code + " " + error.message);
+            }
         });
     }
 	
