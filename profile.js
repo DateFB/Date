254f27a2d2932ff176c95cc9e0bd8215ceee974b
Shaun VanWeelden <shaun.thomas.3141@gmail.com>
2015-03-06 22:42:28 -0800

    Trying to get a bunch of info from your fb profile

--- profile.js
+++ profile.js
@@ -1,4 +1,4 @@
-currentUser = undefined;
+currentUser = {};
 
 $("#loginbtn").click(function(){
     
@@ -6,15 +6,89 @@ $("#loginbtn").click(function(){
     
     Parse.FacebookUtils.logIn(null, {
       success: function(user) {
+          
+        currentUser.fbUserAuthData = user;
+        currentUser.fbUserName = user.attributes.username;
+          
         if (!user.existed()) {
-          alert("User signed up and logged in through Facebook!");
+            // show welcome stuff
+            alert("User signed up and logged in through Facebook!");
+            
+            createInitialUser();
+            
         } else {
-          alert("User logged in through Facebook!");
+            // user already has an account
+            alert("User logged in through Facebook!");
         }
-        currentUser = user;
+        
       },
       error: function(user, error) {
         alert("User cancelled the Facebook login or did not fully authorize.");
       }
     });
 })
+
+function createInitialUser(){
+    var UserInformation = Parse.Object.extend("UserInformation");
+    var userInfo = new UserInformation();
+
+    userInfo.set("fbUserName", currentUser.fbUserName);
+
+    userInfo.save(null, {
+      success: function(userInfo) {
+        // a bunch of nested async save calls to get user profile info
+          
+        // get some basic info
+        FB.api(
+            "/me",
+            function (response) {
+                if (response && !response.error) {
+                    // get data from fb
+                    currentUser.firstName = response.first_name;
+                    currentUser.lastName = response.last_name;
+                    currentUser.gender = response.gender;
+                    currentUser.fb_id = response.id;
+                    
+                    
+                    userInfo.set("firstName", currentUser.firstName);
+                    userInfo.set("lastName",currentUser.lastName);
+                    userInfo.set("gender", currentUser.gender);
+                    userInfo.set("fb_id",currentUser.fb_id);
+                    userInfo.save();
+                    
+                    // get profile pic from fb
+                    FB.api(
+                        "/me/picture",
+                        {
+                            "redirect": false,
+                            "height": 200,
+                            "width": 200,
+                            "type": "normal"
+                        },
+                        function (response) {
+                            if (response && !response.error) {
+                                currentUser.profilePic = response.data.url;
+                                userInfo.set("profilePic", currentUser.profilePic);
+                                userInfo.save();
+                            }
+                            else{
+                                console.log("Had trouble accessing FB API (#2)");
+                            }
+                        }
+                    );
+                }
+                else{
+                    console.log("Had trouble accessing FB API (#1)");
+                }
+            }
+        );
+        
+          
+      },
+      error: function(userInfo, error) {
+        // Execute any logic that should take place if the save fails.
+        // error is a Parse.Error with an error code and message.
+        alert('Failed to create new object, with error code: ' + error.message);
+      }
+    });
+}
