bb46bca445bbdc87575d397e0c4f34dcb6cf0a19
Joshua Nazarian <jknaz@cs.washington.edu>
2015-03-07 22:47:09 -0800

    createDate.js

--- createDate.js
+++ createDate.js
@@ -6,9 +6,8 @@
     });
 
     function getLocation() {
-        if (navigator.geolocation) {
+        if (navigator.geolocation)
             navigator.geolocation.getCurrentPosition(displayMap);
-        }
     }
 
     var gmap;
@@ -35,25 +34,23 @@
     }
 
     function updateMap() {
-        for (var i = 0; i < markers.length; i++) {
+        for (var i = 0; i < markers.length; i++)
             markers[i].setMap(null);
-        }
+
         markers = [];
         var cat = catBox.options[catBox.selectedIndex];
         var request = {
             location: focus,
-            radius: 4800,
+            radius: '4800',
             types: ["" + cat.value]
         };
         service.nearbySearch(request, callback);
     }
 
     function callback(results, status) {
-        if (status == google.maps.places.PlacesServiceStatus.OK) {
-            for (var i = 0; i < results.length; i++) {
+        if (status == google.maps.places.PlacesServiceStatus.OK)
+            for (var i = 0; i < results.length; i++)
                 createMarker(results[i]);
-            }
-        }
     }
 
     function createMarker(place) {
@@ -63,12 +60,16 @@
         });
         markers.push(marker);
         google.maps.event.addListener(marker, 'click', function() {
-            infowindow.setContent(place.name + " - " + place.formatted_address);
+            if (place.formatted_address != undefined)
+                infowindow.setContent(place.name + " - " + place.formatted_address);
+            else
+                infowindow.setContent(place.name);
             infowindow.open(gmap, this);
         });
     }
 
     function makeDateInfo() {
+        // get values from form fields
         var uid = Parse.User.current().attributes.username;
         var headline = document.getElementById("title").value;
         var locationName = document.getElementById("locationName").value;
@@ -76,17 +77,19 @@
         var purpose = catBox.options[catBox.selectedIndex].value;
         var desc = document.getElementById("description").innerHTML;
 
-
+        // query the current user's information
         var userInfo;
         var currentUser = Parse.User.current();
         var UserInformationClass = Parse.Object.extend("UserInformation");
         var query = new Parse.Query(UserInformationClass);
         query.equalTo("fbUserName", currentUser.attributes.username);
-        var age = 0;
-        var gender = "";
-        var seekmin = 0;
-        var seekmax = 100;
-        var seekgen = "";
+
+        var age;
+        var gender;
+        var seekmin;
+        var seekmax;
+        var seekgen;
+
         query.find({
             success: function(results) {
                 userInfo = results[0];
@@ -101,6 +104,7 @@
             }
         });
 
+        // date and time (need to fix hour to +12 on pm)
         var date = document.getElementById("datepicker").value;
         var p0 = date.split('-');
         var time = document.getElementById("timepicker").value;
@@ -108,6 +112,7 @@
         var p2 = p1[0].split(':');
         var datetime = new Date(p0[0], p0[1], p0[2], p2[0], p2[1]);
 
+        // need to change this to save location of selected place
         var locGeo = new Parse.GeoPoint({latitude: position.coords.latitude, longitude: position.coords.longitude});
 
         saveDateInfo(uid, datetime, headline, locationName, locGeo, blind, purpose, desc, age, gender, seekmin, seekmax, seekgen);
