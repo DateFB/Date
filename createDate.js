7dd537e9a4cf81f7d545041906b07537d4eff26b
Joshua Nazarian <jknaz@cs.washington.edu>
2015-03-07 22:59:45 -0800

    createDate.js

--- createDate.js
+++ createDate.js
@@ -121,5 +121,37 @@
         saveDateInfo(uid, datetime, headline, locationName, locGeo, blind, purpose, desc, age, gender, seekmin, seekmax, seekgen);
     }
 
+    function saveDateInfo(uid, datetime, headline, locName, locGeo, blind, purpose,
+        desc, age, gender, seekmin, seekmax, seekgen) {
+        var DateInfo = Parse.Object.extend("DateInfo");
+        var dateinfo = new DateInfo();
+
+        dateinfo.set("UserID", uid);
+        dateinfo.set("DateString", new Date());
+        dateinfo.set("Headline", headline);
+        dateinfo.set("LocationName", locName);
+
+        // var locGeo = new Parse.GeoPoint({latitude: -40, longitude: 120});
+        dateinfo.set("LocationGeo", locGeo);
+
+        dateinfo.set("Blind", blind);
+        dateinfo.set("Purpose", purpose);
+        dateinfo.set("Description", desc);
+
+        dateinfo.set("PosterAge", age);
+        dateinfo.set("PosterGender", gender);
+
+        dateinfo.set("PosterMinAgePref", seekmin);
+        dateinfo.set("PosterMaxAgePref", seekmax);
+        dateinfo.set("PosterGenderPref", seekgen);
+
+        dateinfo.save(null, {
+            success: function(dateinfo) { },
+            error: function(dateinfo, error) {
+                alert("error connecting to Parse: " + error);
+            }
+        });
+    }
+
     google.maps.event.addDomListener(window, 'load', getLocation);
 // })();
